b4d9dac61375be9516f7d57676c2eada4c1e3801
Craig McNicholas <craig.mcnicholas@yotta.co.uk>
2019-07-05 16:41:28 +0100

    Support for overlays

---
+++ src/map/overlays/AlloyOverlayPositioning.ts
@@ -0,0 +1,14 @@
+/**
+ * the positioning of an overlay
+ */
+export enum AlloyOverlayPositioning {
+  TopLeft = 'top-left',
+  TopCentre = 'top-center',
+  TopRight = 'top-right',
+  CentreLeft = 'center-left',
+  CentreCentre = 'center-center',
+  CentreRight = 'center-right',
+  BottomLeft = 'bottom-left',
+  BottomCentre = 'bottom-center',
+  BottomRight = 'bottom-right',
+}
