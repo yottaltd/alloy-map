11e98a3ecc4bccbc581ad4715410f4ff9639303f
OlegRayskiy <41791122+OlegRayskiy@users.noreply.github.com>
2019-12-20 15:49:34 +0000

    AL-4930 - map tests (#57)
    * AL-4934 - updated openlayers to v6
    
    * geometry types cleanup
    
    * coordinate import fix
    
    * AL-4930 - feature tests
    
    * Test split
    
    * clean up
    
    * typescript update
    
    * imports fix
    
    * removed EnumGuards file
    
    * Update src/map/core/AlloyMap.ts
    
    * Update src/map/core/AlloyMap.ts
    
    Co-authored-by: Craig McNicholas <41394833+cmcnicholas@users.noreply.github.com>

---
+++ cypress/specs/MapData.ts
@@ -0,0 +1,29 @@
+import { AlloyMap } from '../../src/map/core/AlloyMap';
+import { AlloyCoordinate } from '../../src/map/core/AlloyCoordinate';
+
+const fixture = 'cypress/specs/_defaults.json';
+
+export default class MapData {
+  private mapObject: AlloyMap | undefined;
+
+  public get map(): AlloyMap {
+    if (!this.mapObject) {
+      throw new Error('failed to get map object');
+    }
+    return this.mapObject;
+  }
+
+  public setup() {
+    cy.readFile(fixture).then((data: any) => {
+      cy.get('#map').then((el) => {
+        this.mapObject = new AlloyMap({
+          api: data.apiUrl,
+          token: data.apiToken,
+          element: el[0],
+          centre: new AlloyCoordinate(data.ukLon, data.ukLat),
+          zoom: data.mapZoom,
+        });
+      });
+    });
+  }
+}
---
+++ cypress/specs/all/navigation.ts
@@ -0,0 +1,92 @@
+import { assert } from 'chai';
+import { AlloyBounds } from '../../../src/map/core/AlloyBounds';
+import { AlloyCoordinate } from '../../../src/map/core/AlloyCoordinate';
+import { MapChangeCentreEvent } from '../../../src/map/events/MapChangeCentreEvent';
+import { MapChangeZoomEvent } from '../../../src/map/events/MapChangeZoomEvent';
+import MapData from '../MapData';
+
+let mapCentre: AlloyCoordinate;
+let mapZoom: number;
+
+export default function(mapData: MapData) {
+  describe('navigation', () => {
+    beforeEach(() => {
+      mapCentre = mapData.map.centre;
+      mapZoom = mapData.map.zoom;
+    });
+
+    it('should set centre and trigger centre event', () => {
+      // test data
+      const coordinate = new AlloyCoordinate(mapCentre.lon - 1, mapCentre.lat + 1);
+      const expected = new AlloyCoordinate(mapCentre.lon - 1, mapCentre.lat + 1);
+      let event = new MapChangeCentreEvent(new AlloyCoordinate(0, 0), 0, [0, 0, 0, 0]);
+
+      // listen to change event
+      mapData.map.addMapChangeCentreListener((e) => {
+        event = e;
+      });
+
+      // update the centre
+      mapData.map.setCentre(coordinate);
+
+      // events exposed via the map instance are debounced
+      cy.wait(500).then(() => {
+        // check the event and property is updated
+        assert.isTrue(event.centre.equals(expected));
+        assert.isTrue(mapData.map.centre.equals(expected));
+      });
+    });
+
+    it('should set zoom and trigger zoom event', () => {
+      // test data
+      const zoom = mapZoom + 2;
+      const expected = mapZoom + 2;
+      let event = new MapChangeZoomEvent(0, 0);
+
+      // listen to change event
+      mapData.map.addMapChangeZoomListener((e) => {
+        event = e;
+      });
+
+      // update the zoom
+      mapData.map.setZoom(zoom);
+      // events exposed via the map instance are debounced
+      cy.wait(500).then(() => {
+        // check the event and property is updated
+        assert.notEqual(event.zoom, mapZoom);
+        assert.equal(event.zoom, expected);
+        assert.equal(mapData.map.zoom, expected);
+      });
+    });
+
+    it('should set viewport and trigger centre/zoom event', () => {
+      // test data
+      const bounds = new AlloyBounds(
+        new AlloyCoordinate(mapCentre.lon, mapCentre.lat),
+        new AlloyCoordinate(mapCentre.lon + 2, mapCentre.lat + 2),
+      );
+      let centreEvent = new MapChangeCentreEvent(new AlloyCoordinate(0, 0), 0, [0, 0, 0, 0]);
+      let zoomEvent = new MapChangeZoomEvent(0, 0);
+
+      // listen to change event
+      mapData.map.addMapChangeCentreListener((e) => {
+        centreEvent = e;
+      });
+      mapData.map.addMapChangeZoomListener((e) => {
+        zoomEvent = e;
+      });
+
+      // update the viewport
+      mapData.map.setViewport(bounds);
+
+      // events exposed via the map instance are debounced
+      cy.wait(500).then(() => {
+        // check the event and property is updated
+        assert.isNotTrue(centreEvent.centre.equals(mapCentre));
+        assert.isNotTrue(mapData.map.centre.equals(mapCentre));
+        assert.notEqual(zoomEvent.zoom, mapZoom);
+        assert.notEqual(mapData.map.zoom, mapZoom);
+      });
+    });
+  });
+}
